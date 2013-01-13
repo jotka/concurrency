8736699ca866951cb267a2043cb157588d7964eb
Jarek Krochmalski <jarek@kpro.pl>
2013-01-13 14:47:14 +0100

    init

---
+++ src/main/java/jotka/pl/CyclicBarrierExample.java
@@ -0,0 +1,38 @@
+package jotka.pl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jarek
+ * Date: 13.01.13
+ * Time: 14:41
+ */
+public class CyclicBarrierExample {
+    private static Logger logger = LoggerFactory.getLogger("CyclicBarrierExample");
+
+    public static void main(String args[]) {
+
+        //creating CyclicBarrier with 3 parties i.e. 3 Threads needs to call await()
+        final CyclicBarrier cb = new CyclicBarrier(3, new Runnable() {
+            @Override
+            public void run() {
+                //This task will be executed once all thread reaches barrier
+                logger.info("All parties are arrived at barrier, lets play");
+            }
+        });
+
+        //starting each of thread
+        Thread t1 = new Thread(new Task(cb, logger), "Thread 1");
+        Thread t2 = new Thread(new Task(cb, logger), "Thread 2");
+        Thread t3 = new Thread(new Task(cb, logger), "Thread 3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
+}
---
+++ src/main/java/jotka/pl/Task.java
@@ -0,0 +1,36 @@
+package jotka.pl;
+
+import org.slf4j.Logger;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jarek
+ * Date: 13.01.13
+ * Time: 14:42
+ */
+public class Task implements Runnable {
+
+    private CyclicBarrier barrier;
+    private final Logger logger;
+
+    public Task(CyclicBarrier barrier, Logger logger) {
+        this.barrier = barrier;
+        this.logger = logger;
+    }
+
+    @Override
+    public void run() {
+        try {
+            logger.info(Thread.currentThread().getName() + " is waiting on barrier");
+            barrier.await();
+            logger.info(Thread.currentThread().getName() + " has crossed the barrier");
+        } catch (InterruptedException ex) {
+            logger.error("Task run InterruptedException ", ex);
+        } catch (BrokenBarrierException ex) {
+            logger.error("Task run BrokenBarrierException", ex);
+        }
+    }
+}
